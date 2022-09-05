4aa453e4be721ca878a6808de054b322e041de17
EMA <emelyapraez25@gmail.com>
2022-09-05 08:56:57 -0500

    blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+//box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+//box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px;
+//box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+
 /* --- COLORES MENU ---*/
 
 
