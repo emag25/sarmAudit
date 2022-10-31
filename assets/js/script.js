eda9a538e24d7f1b7d50dfc9cf9280f64a07257d
EMA <emelyapraez25@gmail.com>
2022-10-31 15:23:42 -0500

    Detalles
    Letras del menu, efectos al hacer scroll, subrayado a la mitad y link de email

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,23 @@
 //box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px;
 //box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
 
-/* --- COLORES MENU ---*/
+/* --- EFECTOS AL HACER SCROLL ---*/
 
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    console.log(screenSize+"  -  "+elements);
+    
+    for(var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
+        if(element.getBoundingClientRect().top < screenSize) {
+            element.classList.add('visible');
+        } else {
+            element.classList.remove('visible');
+        }
+    }
+});
 
 
 
